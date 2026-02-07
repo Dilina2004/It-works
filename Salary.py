300ef721b092b9dcf5e92f883d2e48c6b51fba3b
Dilina Rathnayaka <dilina20051125@gmail.com>
2026-02-07 22:19:55 +0530

    salary

---
+++ Salary.py
@@ -0,0 +1,19 @@
+class Salary:
+    def __init__(self,basic,allowance):
+        self.basic=basic
+        self.allowance=allowance
+
+    def calculate_net_salary(self):
+        return self.basic+self.allowance
+    
+basic_salary=input("Enter Basic Salary= ")
+allowance=input("Enter Allowance= ")
+
+if basic_salary.replace(".","").isdigit() and allowance.replace(".","").isdigit():
+    basic_salary=float(basic_salary)
+    allowance=float(allowance)
+
+salary=Salary(basic_salary,allowance)
+net_salary=salary.calculate_net_salary()
+
+print("Net Salary= ",net_salary)
