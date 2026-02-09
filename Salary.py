0e1deb47b1ffc480094247928be4a4e6ac7806db
anjanakawshan089@gmail.com <anjanakawshan089@gmail.com>
2026-02-09 08:42:56 +0530

    added main

--- Salary.py
+++ Salary.py
@@ -6,14 +6,18 @@ class Salary:
     def calculate_net_salary(self):
         return self.basic+self.allowance
     
-basic_salary=input("Enter Basic Salary= ")
-allowance=input("Enter Allowance= ")
+def main():  
+    basic_salary=input("Enter Basic Salary= ")
+    allowance=input("Enter Allowance= ")
 
-if basic_salary.replace(".","").isdigit() and allowance.replace(".","").isdigit():
-    basic_salary=float(basic_salary)
-    allowance=float(allowance)
+    if basic_salary.replace(".","").isdigit() and allowance.replace(".","").isdigit():
+        basic_salary=float(basic_salary)
+        allowance=float(allowance)
 
-salary=Salary(basic_salary,allowance)
-net_salary=salary.calculate_net_salary()
+    salary=Salary(basic_salary,allowance)
+    net_salary=salary.calculate_net_salary()
 
-print("Net Salary= ",net_salary)
+    print("Net Salary= ",net_salary)
+
+if __name__=="__main__":
+    main()
